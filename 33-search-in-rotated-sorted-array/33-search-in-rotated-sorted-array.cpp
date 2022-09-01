dca20575311d09b6f7d630fc3271a901f2b8de2c
Nishant Shingate <62829800+Nishant0073@users.noreply.github.com>
2022-09-01 10:46:24 +0530

    Time: 7 ms (54.03%), Space: 11.1 MB (29.35%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -9,14 +9,14 @@ public:
                 return m;
             if(nums[l]<=nums[m])
             {
-                if(k>=nums[l] && k<=nums[m])
+                if(k>=nums[l] && k<nums[m])
                     r=m-1;
                 else
                     l=m+1;
             }
             else
             {
-                if(k>=nums[m] && nums[r]>=k)
+                if(k>nums[m] && nums[r]>=k)
                     l=m+1;
                 else
                     r=m-1;
