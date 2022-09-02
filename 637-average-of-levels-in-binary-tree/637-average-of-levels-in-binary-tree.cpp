77be0f44681fb9069ddb6a90de8da4335229c7a7
Nishant Shingate <62829800+Nishant0073@users.noreply.github.com>
2022-09-02 10:26:05 +0530

    Time: 25 ms (52.20%), Space: 22.5 MB (32.47%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
@@ -12,34 +12,27 @@
 class Solution {
 public:
     vector<double> averageOfLevels(TreeNode* root) {
-        map<int,vector<double>>mp;
-        queue<pair<TreeNode*,int>>q;
-        q.push({root,1});
-        while(q.size()!=0)
-        {
-            auto top = q.front();
-            q.pop();
-            mp[top.second].push_back(top.first->val);
-            if(top.first->left!=NULL)
-                q.push({top.first->left,top.second+1});
-            if(top.first->right!=NULL)
-                q.push({top.first->right,top.second+1});
-        }
         
-        vector<double> ans;
-        for(auto it:mp)
+        vector<double>v;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
         {
-            int sum  = 0;
-            double n = it.second.size();
-            double tmp = 0;
-            for(auto i:it.second)
+            double qsize = q.size();
+            double sum = 0;
+            for(int i=0;i<qsize;i++)
             {
-                tmp+=i;
+                auto frnt = q.front();
+                q.pop();
+                sum+=frnt->val;
+                if(frnt->left)
+                    q.push(frnt->left);
+                if(frnt->right)
+                    q.push(frnt->right);
             }
-            tmp = tmp/n;
-            ans.push_back(tmp);
+            v.push_back(sum/qsize);
         }
-        return ans;
+        return v;
         
     }
 };
