5a5f577799aea233e8a9b2331db2840cc112fd81
Nishant Shingate <62829800+Nishant0073@users.noreply.github.com>
2022-09-19 18:29:34 +0530

    Time: 59 ms (35.59%), Space: 25.1 MB (58.55%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -12,7 +12,7 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k%n;
-        reverse(nums.begin(),nums.end());
+        halfRotate(nums,0,n-1);
         halfRotate(nums,0,k-1);
         halfRotate(nums,k,n-1);
     }
