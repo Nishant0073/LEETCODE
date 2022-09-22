af33ec08d0d0bc10d7edd36c228064ce798a7bda
Nishant Shingate <62829800+Nishant0073@users.noreply.github.com>
2022-09-22 08:31:53 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (23.66%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -4,14 +4,14 @@ public:
         int cnt0 = 0;
         int cnt1 = 0;
         int cnt2 = 0;
-        for(int i=0;i<nums.size();i=i+1)
+        for(int i=0;i<nums.size();i++)
         {
             if(nums[i]==0)
-                cnt0 = cnt0+1;
+                cnt0++;
             if(nums[i]==1)
-                cnt1 = cnt1+1;
+                cnt1++;
             if(nums[i]==2)
-                cnt2 = cnt2+1;
+                cnt2++;
         }
         int i=0;
         while(cnt0--)
