615d8de9507c0bc69156847ac0c82f9ac2d1baef
Nishant Shingate <62829800+Nishant0073@users.noreply.github.com>
2022-10-04 14:37:51 +0530

    Time: 3 ms (85.50%), Space: 7.1 MB (51.94%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int n = s.size();
+        int i=0;
+        int num = 0;
+         bool isng = false;
+        while(i<n && s[i]==' ')
+            i++;
+        if(i<n && (s[i]=='-' || s[i]== '+' || s[i]>='0' && s[i]<='9'))
+        {
+           
+            if((s[i]=='-' || s[i]=='+'))
+            {
+                if(s[i]=='-')
+                    isng = true;
+                i++;
+            }
+            while(i<n && s[i]>='0' && s[i]<='9')
+            {
+                 if(num>(INT_MAX-(s[i]-'0'))/10)
+                {
+                    if(isng)
+                    {
+                        return(-1-(INT_MAX));
+                    }
+                    return (INT_MAX);
+                }
+               num = (num*10+(s[i++]-'0')) ;
+            }
+        }
+        
+            if(isng)
+                num*=-1;
+            return num;
+    }
+};
