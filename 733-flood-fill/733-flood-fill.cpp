e537f3cfcd61e389cfa97592e3bc0ee806540b03
Nishant Shingate <62829800+Nishant0073@users.noreply.github.com>
2022-10-09 17:49:39 +0530

    Time: 19 ms (42.72%), Space: 14.5 MB (12.26%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n = image.size();
+        int m = image[0].size();
+        
+        if(image[sr][sc]==color)
+            return image;
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
+        queue<pair<int,int>> que;
+        que.push({sr,sc});
+        vis[sr][sc] = true;
+        int repcolor = image[sr][sc];
+        int delr[] = {-1,1,0,0};
+        int delc[] = {0,0,-1,1};
+        while(!que.empty())
+        {
+            auto frnt = que.front();
+            que.pop();
+            int r = frnt.first;
+            int c = frnt.second;
+            image[r][c] = color;
+            for(int i=0;i<4;i++)
+            {
+                int nowr = r+delr[i];
+                int nowc = c+delc[i];
+            if(nowr>=0 &&  nowr<n && nowc<m && nowc>=0 && image[nowr][nowc]==repcolor && !vis[nowr][nowc]){
+                    vis[nowr][nowc] = true;
+                    que.push({nowr,nowc});
+                }
+            }
+            
+        }
+        return image;
+    }
+};
