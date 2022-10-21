f435c1c4a078ce97fbbdf9b828358dd1e75ff9c8
Nishant Shingate <62829800+Nishant0073@users.noreply.github.com>
2022-10-21 12:08:45 +0530

    Time: 35 ms (77.78%), Space: 17.3 MB (7.71%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -1,15 +1,41 @@
 class NumArray {
 public:
-    vector<int> pref;
+    vector<int> sgt;
+    vector<int> arr;
+    int n;
     NumArray(vector<int>& nums) {
-        pref.push_back(0);
-        for(auto i=0;i<nums.size();i++)
-            pref.push_back(pref.back()+nums[i]);
+        n = nums.size();
+        sgt = vector<int>(n*4,0);
+        arr = nums;
+        build(0,n-1,0);
+    }
+    
+    void build(int l,int r,int v){
+        if(l==r)
+            sgt[v] = arr[l];
+        else
+        {
+            int m = (l+r)/2;
+            build(l,m,v*2+1);
+            build(m+1,r,v*2+2);
+            sgt[v] = sgt[v*2+1]+sgt[v*2+2];
+        }
+    }
+    
+    int getSum(int l,int r,int ql,int qr,int v)
+    {
+        if(l>qr || r<ql)
+            return 0;
+        if(ql<=l && r<=qr)
+            return sgt[v];
+        int m = (l+r)/2;
+        
+        return (getSum(l,m,ql,qr,v*2+1)+getSum(m+1,r,ql,qr,v*2+2));
         
     }
     
     int sumRange(int left, int right) {
-        return (pref[right+1]-pref[left]);
+        return getSum(0,n-1,left,right,0);
     }
 };
 
