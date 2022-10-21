71221d2c40b16a16568046b5945825c67a07d70a
Nishant Shingate <62829800+Nishant0073@users.noreply.github.com>
2022-10-21 09:00:00 +0530

    Time: 603 ms (5.88%), Space: 80.4 MB (15.96%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -5,7 +5,7 @@ public:
         int n = nums.size();
         for(int i=0;i<n && i<=k;i++)
         {
-            mp[nums[i]]++;
+            mp[nums[i]] = mp[nums[i]]+1;
             if(mp[nums[i]]>=2)
                 return true;
         }
@@ -14,8 +14,8 @@ public:
         
         for(int i=k+1;i<n;i++)
         {
-            mp[nums[i-k-1]]--;
-            mp[nums[i]]++;
+            mp[nums[i-k-1]] = mp[nums[i-k-1]]-1;
+            mp[nums[i]] = mp[nums[i]] +1;
             if(mp[nums[i]]>=2)
                 return true;
         }
